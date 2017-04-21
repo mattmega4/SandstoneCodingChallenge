34c82caa1d2aa379c3746fb1ca8bf91d0b2ca95e
mattmega4 <matthewhsingleton@Matthews-MacBook-Pro.local>
2017-04-20 22:30:24 -0700

    Clean up code, moved delegate methods to extension, added MARKS

--- SandstoneCodingChallenge/MainViewController.swift
+++ SandstoneCodingChallenge/MainViewController.swift
@@ -9,27 +9,19 @@
 import UIKit
 import PureLayout
 
-class MainViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class MainViewController: UIViewController {
     
-    
-   
     @IBOutlet weak var imgView: UIImageView!
-
-   
-    
     @IBOutlet weak var chooseFromPhotoLibraryButton: UIButton!
     @IBOutlet weak var clearButton: UIButton!
     @IBOutlet weak var takePhotoButton: UIButton!
-    
     @IBOutlet weak var decreaseLineSizeButton: UIButton!
     @IBOutlet weak var lineLenghLabel: UILabel!
-
     @IBOutlet weak var increaseLineSizeTextField: UIButton!
     
     let path = UIBezierPath()
     let shapeLayer = CAShapeLayer()
-    
-    var imagePicker: UIImagePickerController!
+    var imagePicker = UIImagePickerController()
     
     var endingPoint = 25
     
@@ -39,61 +31,10 @@ class MainViewController: UIViewController, UINavigationControllerDelegate, UIIm
         
         settingUpViewUsingAPI()
         drawLineFromPoint(end: endingPoint, inView: imgView)
-        
-    }
-    
-
-    func drawLineFromPoint(end: Int, inView view: UIView) {
-        
-        
-        
-        //design the path
-        
-        path.move(to: CGPoint(x: 25, y: 150))
-        path.addLine(to: CGPoint(x: end, y: 150))
-
-        
-        //design path in layer
-        
-        shapeLayer.path = path.cgPath
-        shapeLayer.strokeColor = UIColor.blue.cgColor
-        shapeLayer.lineWidth = 5.0
-        
-        
-        view.layer.addSublayer(shapeLayer)
-        
-        
-        let currentLineLength = end - 25
-        
-        lineLenghLabel.text = "\(currentLineLength) Pixels Long"
-        print(endingPoint)
-        print("bar")
-        
     }
     
     
-
-    
-    
-    
-    
-    
-    
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        
-        guard let chosenImg = info[UIImagePickerControllerOriginalImage] as? UIImage else {
-            print("error")
-            return
-        }
-
-        
-        imgView.image = chosenImg
-        UIImageWriteToSavedPhotosAlbum(chosenImg, nil, nil, nil)
-        imagePicker.dismiss(animated: true, completion: nil)
-    }
-    
-    
+    // MARK: PureLayout View Setup
     
     func settingUpViewUsingAPI() {
         
@@ -106,8 +47,6 @@ class MainViewController: UIViewController, UINavigationControllerDelegate, UIIm
         imgView.autoPinEdge(toSuperviewEdge: .left, withInset: 20.0)
         imgView.autoPinEdge(toSuperviewEdge: .right, withInset: 20.0)
         
-
-        
         let topButtonArray: NSArray = [chooseFromPhotoLibraryButton, clearButton, takePhotoButton]
         topButtonArray.autoDistributeViews(along: .horizontal, alignedTo: .horizontal, withFixedSpacing: 10.0, insetSpacing: true, matchedSizes: true)
         topButtonArray.autoSetViewsDimension(.height, toSize: 30.0)
@@ -115,8 +54,6 @@ class MainViewController: UIViewController, UINavigationControllerDelegate, UIIm
         chooseFromPhotoLibraryButton.autoPinEdge(.top, to: .bottom, of: imgView, withOffset: 25.0)
         clearButton.autoPinEdge(.top, to: .bottom, of: imgView, withOffset: 25.0)
         takePhotoButton.autoPinEdge(.top, to: .bottom, of: imgView, withOffset: 25.0)
-        
-
         
         decreaseLineSizeButton.autoPinEdge(.top, to: .bottom, of: chooseFromPhotoLibraryButton, withOffset: 25.0)
         decreaseLineSizeButton.autoPinEdge(.top, to: .bottom, of: clearButton, withOffset: 25.0)
@@ -144,23 +81,29 @@ class MainViewController: UIViewController, UINavigationControllerDelegate, UIIm
     }
     
     
+    // MARK: Draw Line Logic
     
-
+    func drawLineFromPoint(end: Int, inView view: UIView) {
+        
+        path.move(to: CGPoint(x: 25, y: 150))
+        path.addLine(to: CGPoint(x: end, y: 150))
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeColor = UIColor.blue.cgColor
+        shapeLayer.lineWidth = 5.0
+        view.layer.addSublayer(shapeLayer)
+        
+        let currentLineLength = end - 25
+        lineLenghLabel.text = "\(currentLineLength) Pixels Long"
+    }
     
-
     
-    
-    
-    
-    
-    
+    // MARK: IBActions
     
     @IBAction func choseFromPhotoLibraryTapped(_ sender: UIButton) {
         imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
-        
     }
     
     @IBAction func clearButtonTapped(_ sender: UIButton) {
@@ -169,31 +112,42 @@ class MainViewController: UIViewController, UINavigationControllerDelegate, UIIm
     }
     
     @IBAction func takePhotoButtonTapped(_ sender: UIButton) {
-        
         imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
         present(imagePicker, animated: true, completion: nil)
-        
     }
-    
     
     @IBAction func decreaseLineSizeButtonTapped(_ sender: UIButton) {
         path.removeAllPoints()
         endingPoint-=1
         drawLineFromPoint(end: endingPoint, inView: imgView)
-        print(endingPoint)
-        print("away")
     }
     
     @IBAction func increaseLineSizeButtonTapped(_ sender: UIButton) {
         path.removeAllPoints()
         endingPoint+=1
         drawLineFromPoint(end: endingPoint, inView: imgView)
-        print(endingPoint)
-        print("go")
     }
     
+} // End of MainViewController Class
+
+
+// MARK: Extensions
+
+extension MainViewController : UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        guard let chosenImg = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            print("error")
+            return
+        }
+        
+        imgView.image = chosenImg
+        UIImageWriteToSavedPhotosAlbum(chosenImg, nil, nil, nil)
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 }
 
 
